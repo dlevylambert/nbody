10a0b6269136615417b016db1ab83ac701654861
yqlu <yqlu@mit.edu>
2014-02-02 12:29:16 -0500

    trying again

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <fstream>
 
-int main() {
+/*int main() {
   try {
     std::ifstream input{ "resources/nbody/binary-system-simple.txt" };
     nbody::Simulation sim{input};
@@ -18,4 +18,24 @@ int main() {
     std::cerr << "Error: " << e.what() << "\n";
     return 1;
   }
+}*/
+
+int TestRiemannIntegrate() {
+	//System sys = new System(1);
+	//float dt = 0;
+	//Riemann riemann = new Riemann();
+	//riemann.Integrate(sys, dt);
+	//boolean position = (int) sys.body(i).position().x() == 0 && (int) sys.body(i).position().y() == 0 && (int) sys.body(i).position().z() == 0;
+	//boolean velocity = (int) sys.body(i).velocity().x() == 0 && (int) sys.body(i).velocity().y() == 0 && (int) sys.body(i).velocity().z() == 0;
+	//if (position && velocity) {
+	//return 0;
+	//}
+	//else {
+	//return 1;
+	//}
+	return 0;
+}
+
+int main() {
+  std::cout << "Test 1: " << TestRiemannIntegrate() << "\n";
 }
